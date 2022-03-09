1259aebf81bab848b4a233f191ad7a04711086da
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2022-03-09 16:37:25 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+import requests
+import shutil
+import subprocess
+import os
 
 app = FastAPI()
 
@@ -13,3 +16,18 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/count-berries")
+def count_berries(image: UploadFile = File(...)):
+
+	path = f"./app/etc/{image.filename}"
+	with open(path, 'wb') as fileimg:
+		fileimg.write(image.file.read())
+
+	return subprocess.run(['python3.9',"./demo/demo.py", 
+                           "--input", path, 
+                           "--output", "grape_from_real", 
+                           "--opts", "MODEL.DEVICE", "cpu",
+                            "MODEL.WEIGHTS", "./training_dir/BoxInst_MS_R_50_1x/model_final.pth"],	
+                          capture_output=True)
